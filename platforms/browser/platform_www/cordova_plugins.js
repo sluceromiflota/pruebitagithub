271495afc41a30037a55c8693ead0919519c4942
slucerob <slucero@miflota.com>
2017-04-18 11:01:22 -0500

    Nuevo proyecto

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/pushbots-cordova-plugin/www/pushbots.js",
+        "id": "pushbots-cordova-plugin.PushbotsPlugin",
+        "pluginId": "pushbots-cordova-plugin",
+        "clobbers": [
+            "PushbotsPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "pushbots-cordova-plugin": "1.4.7",
+    "cordova-plugin-whitelist": "1.3.2"
+}
+// BOTTOM OF METADATA
+});
